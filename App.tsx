c925c745a3da8670d784c8cd3d133f232471e33b
Wellington <wellingtongmb@gmail.com>
2024-10-15 13:37:02 -0300

    add styles in the project

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>React Native</Text>
+      <Text style={styles.eventDate}>Nois aqui!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#FDFCFE",
+    fontSize: 16,
+  },
+});
